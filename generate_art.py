adca1e92d9518a27553f53aca66aed4a454bd935
EmilIvanichkovv <emil.ivanichkov@gmail.com>
2022-06-04 19:10:20 +0300

    improve(): Add interpolating colors to the lines

--- generate_art.py
+++ generate_art.py
@@ -1,11 +1,28 @@
+from curses import start_color
 from PIL import Image, ImageDraw
 import random
+
+def generate_random_color():
+    return(random.randint(0,225),random.randint(0,225),random.randint(0,225))
+
+
+def color_interpolate(start_color, end_color, factor: float):
+    # Find the color that is exactly factor (0.0 - 1.0) between the two colors.
+    new_color_rgb = []
+    for i in range(3):
+        new_color_value = factor * end_color[i] + (1 - factor) * start_color[i]
+        new_color_rgb.append(int(new_color_value))
+
+    return tuple(new_color_rgb)
 
 def generate_art():
     print("Hello world")
     image_size_px = 250
     padding_px = 20
     image_bg_color = (255,255,255)
+    start_color = generate_random_color()
+    end_color = generate_random_color()
+
     image = Image.new(
         "RGB", 
         size=(image_size_px, image_size_px), 
@@ -13,7 +30,6 @@ def generate_art():
     # Draw some lines
 
     draw = ImageDraw.Draw(image)
-    thickness = 0
     points = []
 
     # Generate The points
@@ -23,15 +39,18 @@ def generate_art():
             random.randint(padding_px, image_size_px - padding_px))
         points.append(random_point)
 
+    thickness = 0
+    n_point = len(points) - 1
     for i, point in enumerate(points):
         p1 = point
-        if i == len(points) - 1:
+        if i == n_point:
             p2 = points[0]
         else:
             p2 = points[i-1]
             
         line_xy = (p1,p2)
-        line_color = (0, 0, 0)
+        color_factor = i /n_point
+        line_color = color_interpolate(start_color, end_color, color_factor)
         thickness = random.randint(1,i+1)
         draw.line(line_xy, fill= line_color, width=thickness)
     
